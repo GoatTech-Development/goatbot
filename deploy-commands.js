2b2f76b37fbf1a741ace5117ebd1567f32ae9c18
nealhitzfield <nealhitzfield@gmail.com>
2023-08-26 12:25:17 -0700

    F

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,8 +4,10 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('start').setDescription('SELECT A GAME TO START, NERD'),
-	new SlashCommandBuilder().setName('stop').setDescription('SELECT A GAME TO STOP, NERD')
+	new SlashCommandBuilder().setName('control').setDescription('Start/Stop Gayme Server(s)'),
+	// new SlashCommandBuilder().setName('start').setDescription('SELECT A GAME TO START, NERD'),
+	// new SlashCommandBuilder().setName('stop').setDescription('SELECT A GAME TO STOP, NERD'),
+	new SlashCommandBuilder().setName('info').setDescription('get info')
 ]
 	.map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -18,31 +18,23 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'start') {
+	if (interaction.commandName === 'control') {
 		const row = new MessageActionRow()
 		.addComponents(
 			new MessageButton()
-				.setCustomId('goatheim_start')
-				.setLabel('Start Goatheim')
-				.setStyle('SUCCESS'),
-			new MessageButton()
 				.setCustomId('goatdays_start')
 				.setLabel('Start Goatdays')
-				.setStyle('DANGER'),
-			new MessageButton()
-				.setCustomId('goatcraft_start')
-				.setLabel('Start Goatcraft')
 				.setStyle('PRIMARY'),
 			new MessageButton()
-				.setCustomId('goatfactory_start')
-				.setLabel('Start Goatfactory')
-				.setStyle('SECONDARY'),
+				.setCustomId('goatdays_stop')
+				.setLabel('Stop Goatdays')
+				.setStyle('DANGER'),
 		);
 		const embed = new MessageEmbed()
 			.setColor('#0099ff')
-			.setTitle('Start GoatGame Containers:')
+			.setTitle('Start/Stop 7 Days to Die:')
 			.setURL('https://goattech.net/bots')
-			.setDescription('Click which server to start:');
+			.setDescription('Scam da traders get the shmackers:');
 		await interaction.reply({ 
             content: `yes melt my cpu wawaweewa`, 
             ephemeral: false, 
@@ -51,37 +43,8 @@ client.on('interactionCreate', async interaction => {
         });
 	}
 
-	if (interaction.commandName === 'stop') {
-		const row = new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('goatheim_stop')
-				.setLabel('Stop Goatheim')
-				.setStyle('SUCCESS'),
-			new MessageButton()
-				.setCustomId('goatdays_stop')
-				.setLabel('Stop Goatdays')
-				.setStyle('DANGER'),
-			new MessageButton()
-				.setCustomId('goatcraft_stop')
-				.setLabel('Stop Goatcraft')
-				.setStyle('PRIMARY'),
-			new MessageButton()
-				.setCustomId('goatfactory_stop')
-				.setLabel('Stop Goatfactory')
-				.setStyle('SECONDARY'),
-		);
-		const embed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('Stop GoatGame Containers:')
-			.setURL('https://goattech.net/bots')
-			.setDescription('Click which server to stop:');
-		await interaction.reply({ 
-            content: `ahhh my cpu is finally cooling down...`, 
-            ephemeral: false, 
-            components: [row], 
-			embeds: [embed],
-        });
+	if (interaction.commandName === 'info') {
+		await interaction.reply('no u');
 	}
 });
 
@@ -89,67 +52,33 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isButton()) return;
 
 	if (interaction.customId === "goatdays_start") {
-		await interaction.reply('Started Goatdays, may you scam the traders and chug yucca smoothies');
-		console.log('goatdays start clicked!');
-		exec('cd /home/neal/goatdays && docker-compose up -d');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatheim_start") {
-		await interaction.reply('Started Goatheim, may you vanquish the dastardly deathsquitos');
-		console.log('goatheim start clicked!');
-		exec('cd /home/neal/goatheim && docker-compose up -d');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatcraft_start") {
-		await interaction.reply('Started Goatcraft, may you scam the villagers and abuse the chickens');
-		console.log('goatcraft start clicked!');
-		exec('cd /home/neal/goatcraft && docker-compose up -d');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatfactory_start") {
-		await interaction.reply('Started Goatfactory, may your power grid never crash');
-		console.log('goatfactory start clicked!');
-		exec('cd /home/neal/goatfactory && docker-compose up -d');
-		await wait(300000);
-		await interaction.deleteReply();
+		if (exec('docker ps | grep sdtd')) {
+			await interaction.reply('Server already running, get out there and scam!');
+			await wait(300000);
+			await interaction.deleteReply();
+		}
+		else {
+			await interaction.reply('Starting Goatdays, may you scam the traders and chug yucca smoothies');
+			console.log('goatdays start clicked!');
+			exec('cd /home/neal/goatdays/alpha21 && docker compose up -d');
+			await wait(300000);
+			await interaction.deleteReply();
+		}
 	}
 
 	if (interaction.customId === "goatdays_stop") {
-		await interaction.reply('Stopped Goatdays, Trader Joel rejoices');
-		console.log('goatdays stop clicked!');
-		exec('cd /home/neal/goatdays && docker-compose stop');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatheim_stop") {
-		await interaction.reply('Stopped Goatheim, the greylings rejoice');
-		console.log('goatheim stop clicked!');
-		exec('cd /home/neal/goatheim && docker-compose stop');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatcraft_stop") {
-		await interaction.reply('Stopped Goatcraft, the abused villagers rejoice');
-		console.log('goatcraft stop clicked!');
-		exec('cd /home/neal/goatcraft && docker-compose stop');
-		await wait(300000);
-		await interaction.deleteReply();
-	}
-
-	if (interaction.customId === "goatfactory_stop") {
-		await interaction.reply('Stopped Goatfactory, the power grid rests...');
-		console.log('goatfactory stop clicked!');
-		exec('cd /home/neal/goatfactory && docker-compose stop');
-		await wait(300000);
-		await interaction.deleteReply();
+		if (exec('docker ps | grep sdtd')) {
+			await interaction.reply('Stopping Goatdays, Trader Joel rejoices');
+			console.log('goatdays stop clicked!');
+			exec('cd /home/neal/goatdays/alpha21 && docker compose stop');
+			await wait(300000);
+			await interaction.deleteReply();
+		}
+		else {
+			await interaction.reply('Server already stopped, come back to scam soon!');
+			await wait(300000);
+			await interaction.deleteReply();
+		}
 	}
 });
 
